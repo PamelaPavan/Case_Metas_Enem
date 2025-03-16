149a24c7ba659ac16cb991d78ac55d1f45bc224b
Pâmela Aliny Cleto Pavan <97994995+PamelaPavan@users.noreply.github.com>
2025-03-16 19:59:42 -0300

    fix:arrumando erro em página de aluno detail

--- src/main/webapp/app/entities/aluno/aluno.model.ts
+++ src/main/webapp/app/entities/aluno/aluno.model.ts
@@ -1,6 +1,13 @@
+// aluno.model.ts
+export interface IMeta {
+  area: string;
+  valor: number;
+}
+
 export interface IAluno {
   id: number;
   nome?: string | null;
+  metas?: IMeta[]; // Adicionando a propriedade metas, que será um array de IMeta
 }
 
 export type NewAluno = Omit<IAluno, 'id'> & { id: null };
